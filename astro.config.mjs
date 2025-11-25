0942fd1b626458b3902f0ee2defcc1818a962666
zhd <228507490@qq.com>
2025-11-25 18:33:43 +0800

    修改

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://aiHRD.github.io',
-	base: '/HRD-Blog/',
+	site: 'https://blog.cdyxd.top/',
+	//base: '/HRD-Blog/',
 	trailingSlash: "ignore",
 	integrations: [
 		tailwind({
